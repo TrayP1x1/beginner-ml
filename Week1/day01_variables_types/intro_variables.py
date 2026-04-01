0b6fdc13d54084951245b09a5b43a3bb81c1b72a
TrayP1x1 <trayvaunp@outlook.com>
2026-04-01 23:25:35 +0200

    day 1 completed

---
+++ Week1/day01_variables_types/intro_variables.py
@@ -0,0 +1,15 @@
+name = "Tray"
+age = 23
+wants_the_job = True
+study_hours = 1.5
+number = "42"
+number_to_int, number_to_float = int(number), float(number)
+
+print(f"You use strings to store text, like {name}.\n\
+You use integers to store whole numbers, like {age}.\n\
+You use booleans to store True or False values, like {wants_the_job}.\n\
+You use floats to store decimal numbers, like {study_hours}.\n\
+You can convert strings to integers or floats, like {number_to_int} and {number_to_float}.")
+
+
+

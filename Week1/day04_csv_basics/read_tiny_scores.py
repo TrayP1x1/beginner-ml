36ce6f3b3e16613ce927b99c4c75fa4cdb5b5838
TrayP1x1 <trayvaunp@outlook.com>
2026-04-05 02:46:09 +0200

    halfway done with day04 week1 and also added black to requirements

--- Week1/day04_csv_basics/read_tiny_scores.py
+++ Week1/day04_csv_basics/read_tiny_scores.py
@@ -1,91 +1,44 @@
 import csv
 from pathlib import Path
 
-def csv_reader(r:int= 1):
-   
-    tiny_scores_path = Path(__file__).resolve().parents[2] / "data" / "tiny_scores.csv"
 
-    with tiny_scores_path.open("r", newline="") as file:
+def csv_reader(path, r: int = 1):
+
+    with path.open("r", newline="") as file:
         reader = csv.DictReader(file)
-        print(f'Headers: {reader.fieldnames}')
+        print(f"The column names are: {reader.fieldnames}")
+
+        total_score = 0
+        amount_of_students = 0
 
         for i, row in enumerate(reader):
-            if i >= r:
-                break
-            print(row)
+            if i < r:
+                print(row)
 
+            total_score += float(row["python_score"])
+            amount_of_students += 1
 
+        if amount_of_students > 0:
+            print(f"The average python score is {total_score/amount_of_students:.2f}")
+        else:
+            print("The list has no python scores")
 
+        print(f"The amount of students is {amount_of_students}")
 
 
+def csv_writeer():
+    tiny_scores_path = Path(__file__).resolve().parents[2] / "data" / "tiny_scores.csv"
 
+    with tiny_scores_path.open("w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([])
 
 
+def main():
+    # Path(__file__) is current file, .resolve() is hard file .parents[]
+    tiny_scores_path = Path(__file__).resolve().parents[2] / "data" / "tiny_scores.csv"
+    csv_reader(tiny_scores_path, 3)
 
 
-
-
-
-
-
-def calculator(number_1, number_2):
-    return number_1 + number_2
-
-print(calculator(8, 12))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def main():
-#     csv_reader(3)
-
-# if __name__ == '__main__':
-#     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()

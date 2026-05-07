42c97257c5654d9258b9a3194edb7dd34ba0cede
TrayP1x1 <trayvaunp@outlook.com>
2026-05-07 04:51:24 +0200

    day 06 finished

--- Week1/day06_numpy_basics/numpy_basics.py
+++ Week1/day06_numpy_basics/numpy_basics.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-
-def create_array():
+def create_array() -> np.ndarray:
     array = np.array([1, 2, 3, 4, 5], dtype=int)
     array += 10
     array *= 2
@@ -9,7 +8,7 @@ def create_array():
     return array
 
 
-def create_matrix():
+def create_matrix() -> np.ndarray:
     matrix = np.array(
         [
             [1, 2, 3],
@@ -18,24 +17,32 @@ def create_matrix():
         ]
     )
 
-    print(f"Row 2: {matrix[1,:]}")
-    print(f"Column 1: {matrix[:,0]}")
+    print(f"Row 2: {matrix[1, :]}")
+    print(f"Column 1: {matrix[:, 0]}")
 
     return matrix
 
 
-def array_info(array):
-
+def array_info(array: np.ndarray) -> None:
     print(f"The array is\n{array}")
     print(f"Shape: {array.shape}")
     print(f"dtype: {array.dtype}")
     print(f"Mean: {array.mean()}")
 
 
+def array_mask(array: np.ndarray) -> None:
+    mask = array > 24
+    print(array)
+    print(mask)
+    print(array[mask])
+    print(array[array > 28])
+
+
 def main():
-    create_array()
-    create_matrix()
-    array_info(create_array())
+    # create_array()
+    # create_matrix()
+    # array_info(create_array())
+    array_mask(create_array())
 
 
 if __name__ == "__main__":
